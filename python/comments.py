4d22b0c63679b4f03a9f41d81aa8f213ce6efb87
zadvajr <danielzadva@gmail.com>
2025-04-06 00:24:45 +0100

    python comments

---
+++ python/comments.py
@@ -0,0 +1,11 @@
+# Comments in python are as follows
+# 1. Single line comments starts with a pound symbol #
+# 2. Multiline comments - python does not have a syntax for creating multiline comments, but since python will ignore a string literal that is not assigned to a variable you can use that to create a multiline comments.
+
+"This is a valid comment in python but not multiline"
+
+"""
+This is a valid multiline comments in python
+it is sometimes refered to as docstring
+
+"""
