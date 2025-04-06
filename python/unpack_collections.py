ad4bca44e9b34307da44694287f272594dbb1463
zadvajr <danielzadva@gmail.com>
2025-04-06 01:08:00 +0100

    unpack collections

---
+++ python/unpack_collections.py
@@ -0,0 +1,11 @@
+#In python: if you have a colleciton of values in a list, set or any collection, you can extract the values and assign them to variable at the same time, this is called unpacking.
+
+#for example
+fruits = ["Apple", "Banana", "Cherry"]
+x, y, z = fruits
+
+print("x = ", x)
+print("y = ", y)
+print("z = ", z)
+
+#end
