f859077bf80f8016a735dd71c79ab3c3ff9e8a0a
zadvajr <danielzadva@gmail.com>
2025-04-06 23:36:54 +0100

    python data types

---
+++ python/python_data_types.py
@@ -0,0 +1,12 @@
+# Python Data Typse
+# In programming, data types is a very important concepts.
+# Variables can store data of different types, and different types can do different things.
+# Python has the following data types built-in by default, in these categories:
+# Text Type: str
+# Numeric Type: int, float and complex
+# Sequence Type: list, tupple, and range
+# Mapping Type: dict
+# Set Type: set, frozenset
+# Boolean Type: bool
+# Binary Type: bytes, bytearray, memoryview
+# None Type: NoneType
