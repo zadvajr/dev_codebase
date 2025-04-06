5ebd0fd42b587ab0146628e5d815291aa73d323a
zadvajr <danielzadva@gmail.com>
2025-04-06 01:01:15 +0100

    assign multiple values to multiple variables at the same time

---
+++ python/assign_multiple_values_multiple_variables.py
@@ -0,0 +1,11 @@
+#In python you can assign multiple values to multiple variables at the same time.
+# But the number of the variables must equate the number of values to be assigned.
+
+#Example
+x, y, z = "Apple", "Banana", "Cherry"
+
+print("x = ", x)
+print("y = ", y)
+print("z = ", z)
+
+#end
