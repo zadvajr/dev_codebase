9fe6a943050473ad39868d78182f22cb60d97baa
zadvajr <danielzadva@gmail.com>
2025-04-06 23:41:48 +0100

    getting type of any object in python

---
+++ python/get_type_of_any_object_in_python.py
@@ -0,0 +1,11 @@
+# Python has a built-in function called type
+# The type() function accepts a variable or any object and return its type 
+
+#For example
+name = "Daniel"
+age = "25"
+
+print(type(name))
+print(type(age))
+
+#
