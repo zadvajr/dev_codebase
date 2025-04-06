5f1744071ba8f650dc82f9ef04c75d7955d318ac
zadvajr <danielzadva@gmail.com>
2025-04-06 23:22:18 +0100

    global variables

---
+++ python/global_variables.py
@@ -0,0 +1,13 @@
+# Global variables - are variables created outside of any code block and can be used both inside and outside of a codeblock
+
+#example
+#create a variable outside a function and use it inside a function
+x = "awesome"
+def myfunc():
+    print("Python is ", x)
+
+myfunc()
+
+
+
+#end
