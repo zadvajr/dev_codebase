ab2461e19f2cdcc19a329e04713e12a961826f67
zadvajr <danielzadva@gmail.com>
2025-04-07 23:09:09 +0100

    generating random numbers using random module

---
+++ python/random_number.py
@@ -0,0 +1,11 @@
+# Random Number
+# Python does not have a random() function to generate a random number.
+# But it has a built-in module called random that can be used to generate random number
+
+#Example
+# generates a random number from 1 to 9
+import random as r
+
+print(r.randrange(1,10)) # Generates a random number between 1 and 9 each time the code is runed.
+
+#end
