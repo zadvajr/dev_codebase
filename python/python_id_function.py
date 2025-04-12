b46ba2e96d0d6dd4be72703b7adc88741ee4f123
zadvajr <danielzadva@gmail.com>
2025-04-12 01:14:21 +0100

    id() function

--- python/python_id_function.py
+++ python/python_id_function.py
@@ -1,7 +1,7 @@
 # There is a fuction in python called id() it returns a memory location of a variable
 # For example
-name = "Daniel"
+NAME = "Daniel"
 
-print(id(name)) # prints: 2292197731440
+print(id(NAME)) # prints: 2292197731440
 
 #end
