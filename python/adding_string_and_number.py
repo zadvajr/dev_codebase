eda9bd1c4af5b5a0c8bc871456213dbd222b78b5
zadvajr <danielzadva@gmail.com>
2025-05-04 22:52:19 +0100

    minor fix

--- python/adding_string_and_number.py
+++ python/adding_string_and_number.py
@@ -1,9 +1,9 @@
 #In python if you try to add a number and a string you will generate an error.
 #for example
-age = 36
-greeting = "Welcome, you are "
-print(greeting + age) #this will generate an errror TypeError
+AGE = 36
+GREETING = "Welcome, you are "
+print(GREETING + AGE) #this will generate an errror TypeError
 
 #To overcome the error, convert any of the variable type to match the other.
 # in this case I will convert age into str and join them with + operator
-print(greeting + str(age)) #this wont generate an erro. comment line 5 to run this.
+print(GREETING + str(AGE)) #this wont generate an erro. comment line 5 to run this.
