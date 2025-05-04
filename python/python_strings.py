d4ecfa476cf5d89df9cdf5a12fe7cfbd0ae53e5a
zadvajr <danielzadva@gmail.com>
2025-05-04 23:06:14 +0100

    python string

---
+++ python/python_strings.py
@@ -0,0 +1,22 @@
+"""Python Strings"""
+# Strings in python are either surrounded by single quotation marks or
+# double quotation marks. "hello" and 'hello' are the same in python.
+# You can display a string literal with the print() function
+
+#Example
+print("Hello")
+print('Hello')
+
+#Quotes inside a string
+# You can use a quote inside a string as long as it did not match the one surrounding it.
+# Example
+print("It's alright!")
+print("He is called 'Zadva'")
+print('He loves "Reading"')
+
+#Assign string to a variable
+# Assigning a string to a variable is done with the variable name
+# followed by an equal sign and the string literal
+# Example
+a = "Hello"
+print(a)
