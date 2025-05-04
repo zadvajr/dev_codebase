0fe0c8a79ef0e4c1eee13ca9484664a66f107f02
zadvajr <danielzadva@gmail.com>
2025-05-04 23:39:27 +0100

    slicing

---
+++ python/slicing_strings.py
@@ -0,0 +1,8 @@
+"""Python String Slicing"""
+# You can return a range of characters from a string by using a slice syntax
+# You achieve this by specifying the start index and the end index
+# Separated by colon to return some part of the string
+# Example: To get characters from position 2 to 5 (not included)
+b = "Hello World"
+print(b[2:5]) # Prints llo
+# Note: The first character has an index 0
