ecd0ddbf62edad4e2f0582e5dd896f65d0b237f7
zadvajr <danielzadva@gmail.com>
2025-05-05 23:24:09 +0100

    convert string to uppercase with upper() methond

---
+++ python/modifying_string.py
@@ -0,0 +1,10 @@
+"""Modifying Strings in Python"""
+# Python has set of built-in methods that you can apply on strings.
+
+# Examples
+# You can convert strings to upper case using the method upper()
+# The upper() method returns the string in uppercase it does not change the original string
+txt = "Hello I am Daniel Zadva Jnr"
+print(txt) # Prints: Hello I am Daniel Zadva Jnr
+print(txt.upper()) # Prints: HELLO I AM DANIEL ZADVA JNR
+print(txt) # Still prints: Hello I am Daniel Zadva Jnr
