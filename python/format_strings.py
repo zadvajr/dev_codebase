30385b709c156a11103b59390aaf3a85424345f2
zadvajr <danielzadva@gmail.com>
2025-05-05 23:55:45 +0100

    f-strings

--- python/format_strings.py
+++ python/format_strings.py
@@ -1,5 +1,13 @@
 """Python Format Strings"""
 # As you may know now, you cannot combine or add a string and a number in python
-age = 37
-txt = "Hello my name is Daniel and I am" + age # This line will result in error
+# age = 37
+# txt = "Hello my name is Daniel and I am" + age # This line will result in error
+# print(txt)
+
+# But with string format this is very much possible
+# we can combine strings and numbers by making use of f-string and format() method
+
+# f-strings
+age = 28
+txt = f"I am Daniel and I am {age} year old."
 print(txt)
