065db7bab11493163d8b29af20c63e3111583dba
zadvajr <danielzadva@gmail.com>
2025-05-05 23:52:45 +0100

    string format

--- python/format_strings.py
+++ python/format_strings.py
@@ -1,1 +1,5 @@
 """Python Format Strings"""
+# As you may know now, you cannot combine or add a string and a number in python
+age = 37
+txt = "Hello my name is Daniel and I am" + age # This line will result in error
+print(txt)
