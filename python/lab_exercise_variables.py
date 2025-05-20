f94302f064a2059b93b73ce7e41afbd741fcdfbe
zadvajr <danielzadva@gmail.com>
2025-05-20 22:49:17 +0100

    lab exercise variables

---
+++ python/lab_exercise_variables.py
@@ -0,0 +1,9 @@
+#Lab: Variables
+john = 3
+mary = 5
+adam = 6
+
+print("John =", john, "Mary =", mary, "Adam =", adam) # 3, 5, 6
+
+total_apples = john + mary + adam
+print("Total number of apples:", total_apples)
