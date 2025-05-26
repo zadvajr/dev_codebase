7c9e862b5f114b288e4f33c62b315f166f0cb352
zadvajr <danielzadva@gmail.com>
2025-05-26 21:41:49 +0100

    basic arithmetic

---
+++ python/arithmetic.py
@@ -0,0 +1,15 @@
+# input a float value for variable a here
+a = float(input("Input value for a: "))
+# input a float value for variable b here
+b = float(input("Input value for b: "))
+
+# output the result of addition here
+print("The sum: ", a + b)
+# output the result of subtraction here
+print("The Minus: ", a - b)
+# output the result of multiplication here
+print("The Product: ", a * b)
+# output the result of division here
+print("The division: ", a / b)
+
+print("\nThat's all, folks!")
