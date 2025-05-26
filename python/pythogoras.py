c886c6e2277538b3d88a9db69f541a080e7d9bdb
zadvajr <danielzadva@gmail.com>
2025-05-26 21:14:43 +0100

    pythogoras theorem

---
+++ python/pythogoras.py
@@ -0,0 +1,6 @@
+# pythogoras theorem
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+print("Hypotenuse length is " + str((leg_a ** 2 + leg_b ** 2) ** .5))
+
+#end
