df2e3f7ee1c4056ecad04c86c3afd3ab996b0e3b
zadvajr <danielzadva@gmail.com>
2025-06-15 12:45:48 +0100

    type conversion

---
+++ python/implicit_type_conversion.py
@@ -0,0 +1,10 @@
+"""Implicit Type Conversion Example"""
+FIRST_NUM = 125 # number of type int
+SECOND_NUM = 1.25 #  number of type float
+
+NEW_NUM = FIRST_NUM + SECOND_NUM # adds two numbers of different type and saves it variable NEW_NUM
+
+print("Value:", NEW_NUM) # prints the value of NEW_NUM = 126.25
+print("Type:", type(NEW_NUM)) # prints the type of NEW_NUM = float
+
+#end
